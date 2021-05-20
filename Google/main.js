e22827ba7273879377aceed5f16e1ae3af30b929
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-05-21 01:44:45 +0900

    search bar dropdown

--- Google/main.js
+++ Google/main.js
@@ -1,8 +1,11 @@
-const wapper = document.querySelector(".wrapper");
+const wrapper = document.querySelector(".wrapper");
 const searchBox = document.querySelector(".search_box");
 const searchBar = searchBox.querySelector("input");
 const dropdown = document.querySelector(".dropdown");
 
-searchBar.addEventListener('focus',()=>{
-    dropdown.style.display = 'block';
+searchBar.addEventListener('focus', (e)=>{
+    console.log(wrapper.classList)
+    dropdown.classList.toggle("wrapperopen");
+    wrapper.classList.toggle("open");
+    searchBox.classList.toggle("hidden");
 })
