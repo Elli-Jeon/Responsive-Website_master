852f22e0bd20572cc85ce6deddc6f397e0637b1a
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-06-01 20:09:37 +0900

    ejs template download

--- MBTI/app.js
+++ MBTI/app.js
@@ -4,9 +4,19 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public'))); // "/"로 연결하면 자동으로 "현재_위치/public"으로 이동
+app.set('views',__dirname+'/views'); 
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname,"/views/index.html")); 
+    // res.sendFile(path.join(__dirname,"/views/index.html")); 
+    let context = {
+        title : 'Check your MBTI',
+        whichPage : 'main', 
+        // main : mainpage, Q : question page
+    }
+    res.render("main.ejs", context); 
 })
 
 app.listen("8000", ()=>{
